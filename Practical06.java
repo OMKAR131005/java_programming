4fc652fb32922e14330b4b4efed3ed3c40d806a5
OMKAR131005 <2023bit043@sggs.ac.in>
2025-01-27 22:08:07 +0530

    Update Practical06.java

--- Practical06.java
+++ Practical06.java
@@ -1,9 +1,4 @@
 public class Practical06 {
-    public static void main(String[] args) {
-        Practical06 p=new Practical06();
-//        p.displayPattern2(5);
-        p.displayPattern1(5);
-    }
     public void displayPattern2(int size){
         for(int row=1;row<=size;row++){
             for(int space=1;space<=size-row;space++){
